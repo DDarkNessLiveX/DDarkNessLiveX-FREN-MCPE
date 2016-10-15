78440b4d0e2e504957cd9bb558a49cfe829bbc34
DDarkNessLiveX <alientycraft@gmail.com>
2016-10-15 21:14:27 +0200

    MO-Main.js
    BETA de mon MOD

---
+++ MO-Main.js
@@ -0,0 +1,12 @@
+var X = Math.round(getPlayerX());
+var Y = Math.round(getPlayerY());
+var Z = Math.round(getPlayerZ());
+
+
+function useItem(x, y, z, itemId, blockId, side, itemDamage, blockDamage){
+	if(itemId==702){
+		clientMessage("Tes coordonées");
+		}
+	}
+ModPE.setItem(702,"bous_pos",0,"BousPos");
+Item.addShapedRecipe(702,1,0,["srs","rvr","srs"],["s",1,0,"r",331,0,"v",20,0]);
